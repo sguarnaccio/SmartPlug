0e8a6423b0dacb4c225a7cef1b5291eb22e626c6
GARNACK\sebastian <sguarnaccio@gmail.com>
2017-06-05 22:09:00 -0300

    v2.0
    Reemplazo las librerias APACHE por HTTPURLConnection en
    RestApiServiceHelper

--- mobile/src/main/java/com/example/sebastian/smartplug/services/RestApiServiceHelper.java
+++ mobile/src/main/java/com/example/sebastian/smartplug/services/RestApiServiceHelper.java
@@ -5,17 +5,7 @@ import android.util.Log;
 import com.example.sebastian.smartplug.Processor.RestApiProcessor;
 import com.example.sebastian.smartplug.models.SmartPlug;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -24,10 +14,11 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-//import java.net.HttpURLConnection; //descomentar para usar HTTPURLConnection
-//import java.net.URL; //descomentar para usar HTTPURLConnection
-//import java.io.OutputStream;//descomentar para usar HTTPURLConnection
-//import java.io.OutputStreamWriter;//descomentar para usar HTTPURLConnection
+import java.net.HttpURLConnection; //descomentar para usar HTTPURLConnection
+import java.net.MalformedURLException;
+import java.net.URL; //descomentar para usar HTTPURLConnection
+import java.io.OutputStream;//descomentar para usar HTTPURLConnection
+import java.io.OutputStreamWriter;//descomentar para usar HTTPURLConnection
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,8 +34,13 @@ public class RestApiServiceHelper {
     public List<SmartPlug> GetDevices(String route){
         JSONObject json = null;
         RestApiProcessor processor = new RestApiProcessor();
-        //URL url = new URL(BASE + route);
-        String url = BASE + route;
+        URL url = null;
+        try {
+            url = new URL(BASE + route);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
         json = GET(url);
         Log.d("Pidiendo ", "Devices");
         return processor.toSmartPlugList(json);//toSmartPlug devuelve una lista de SmartPlug objects o un solo elemento dependiendo lo q se pida
@@ -53,39 +49,44 @@ public class RestApiServiceHelper {
     public SmartPlug GetDeviceData(String route){//para cuando se elige un dispositivo en particular
         RestApiProcessor processor = new RestApiProcessor();
         JSONObject json = null;
-        //URL url = new URL(BASE + route);
-        String url = BASE + route;
+        URL url = null;
+        try {
+            url = new URL(BASE + route);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
         json = GET(url);
         return processor.toSmartPlug(json);//toSmartPlug devuelve una lista de SmartPlug objects o un solo elemento dependiendo lo q se pida
     }
 
     public void SendConfig(String route, String payload){
         //JSONObject json = new JSONObject(payload);
-        List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("firstParam", payload));
-        //URL url = new URL(BASE + route);
-        String url = BASE + route;
-        //String response = PUT(url, payload);
-        String response = PUT(url,params);
+
+        URL url = null;
+        try {
+            url = new URL(BASE + route);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        String response = PUT(url, payload);
+
         //TODO tratar la respuesta para ver si pudo enviar el comando o si se desconecto el dispositivo
         return;
     }
 
 
 
-    private JSONObject GET(String url) {
-    //public JSONObject request(URL url){
+    public JSONObject GET(URL url){
         JSONObject json = null;
-        //TODO MIGRAR libreris apache A HTTPURLConnection
-        HttpClient httpClient = new DefaultHttpClient();
-        //HttpPost httpPost = new HttpPost(url);
-        HttpGet httpGet = new HttpGet(url);
+
 
 
         try {
 
-            Log.d(LOG, url);
-            /*Log.d(LOG, url.toString());
+
+            Log.d(LOG, url.toString());
             //setting up http connection
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setReadTimeout(10000);
@@ -111,30 +112,7 @@ public class RestApiServiceHelper {
             }
             conn.disconnect();
             Log.d(LOG, total.toString());
-            json = new JSONObject(total.toString());*/
-
-            httpGet.setHeader("Content-Type", "application/json");
-            //httpPost.setHeader("Content-Type", "application/json");
-            HttpResponse response = httpClient.execute(httpGet);
-            //HttpResponse response = httpClient.execute(httpPost);
-
-            Log.d(LOG, response.getStatusLine().toString());
-
-            HttpEntity entity = response.getEntity();
-
-            if (entity != null) {
-                // A Simple JSON Response Read
-                InputStream instream = entity.getContent();
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(instream));
-                StringBuilder total = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    total.append(line);
-                }
-                Log.d(LOG, total.toString());
-                json = new JSONObject(total.toString());
-            }
+            json = new JSONObject(total.toString());
 
         } catch (ClientProtocolException e) {
             e.printStackTrace();
@@ -147,54 +125,15 @@ public class RestApiServiceHelper {
         return json;
     }
 
-    /*public String requestHtml(String url, List<NameValuePair> params) {
 
-        HttpClient httpClient = new DefaultHttpClient();
-        HttpPost httpPost = new HttpPost(url);
-
-        try {
-            httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
-            UrlEncodedFormEntity query = new UrlEncodedFormEntity(params);
-            httpPost.setEntity(query);
-
-            HttpResponse response = httpClient.execute(httpPost);
+    public String PUT(URL url, String params){
+        JSONObject json = null;
 
 
-            HttpEntity entity = response.getEntity();
-
-            if (entity != null) {
-                // A Simple JSON Response Read
-                InputStream instream = entity.getContent();
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(instream));
-                StringBuilder total = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    total.append(line);
-                }
-                Log.d(LOG, total.toString());
-                return total.toString();
-            }
-
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }*/
-
-    private String PUT(String url, List<NameValuePair> params) {
-    //public String PUT(URL url, String params){
-        //JSONObject json = null;
-        HttpClient httpClient = new DefaultHttpClient();
-
-        HttpPut httpPut = new HttpPut(url);
-        //TODO MIGRAR libreris apache A HTTPURLConnection
 
         try {
 
-            /*Log.d(LOG, url.toString());
+            Log.d(LOG, url.toString());
             //setting up http connection
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setReadTimeout(10000);//10 seg
@@ -230,33 +169,7 @@ public class RestApiServiceHelper {
             }
             conn.disconnect();
             Log.d(LOG, total.toString());
-            json = new JSONObject(total.toString());*/
-
-
-            httpPut.setHeader("Content-Type", "application/json");
-            httpPut.setHeader("Accept", "application/json");
-            UrlEncodedFormEntity query = new UrlEncodedFormEntity(params);
-            httpPut.setEntity(query);
-
-            HttpResponse response = httpClient.execute(httpPut);
-
-
-            HttpEntity entity = response.getEntity();
-
-            if (entity != null) {
-                // A Simple JSON Response Read
-                InputStream instream = entity.getContent();
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(instream));
-                StringBuilder total = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    total.append(line);
-                }
-                Log.d(LOG, total.toString());
-                //json = new JSONObject(total.toString());
-                return total.toString();
-            }
+            //json = new JSONObject(total.toString());
 
         } catch (ClientProtocolException e) {
             e.printStackTrace();
